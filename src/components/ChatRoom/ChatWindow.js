d71c5cd4de70da4426bd51dd61ee0a0f5c75b5a7
taideptrai04 <phuoctai1810@gmail.com>
2024-05-31 00:03:47 +0700

    ChatGroup

--- src/components/ChatRoom/ChatWindow.js
+++ src/components/ChatRoom/ChatWindow.js
@@ -1,30 +1,38 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { SelectedGroupContext } from './SelectedGroupContext';
 import { HomeOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import GroupInfo from '../Modals/GroupInfo';
-export default function ChatWindow() {
-    const { selectedGroup } = useContext(SelectedGroupContext);
-    // const [showGroupInfo, setShowGroupInfo] = useState(false);
-    const [isGroupInfoVisible, setIsGroupInfoVisible] = useState(false);
-    // const handleHomeClick = () => {
-      // setShowGroupInfo(true);
-    // };
-    const handleGroupInfoToggle = () => {
-      setIsGroupInfoVisible(!isGroupInfoVisible);
-    };
-  
+import ChatRoom from './ChatRoom'
+import { doc, onSnapshot, setDoc, arrayUnion, getDoc, updateDoc } from 'firebase/firestore';
+import { db } from '../lib/firebaseConfig';
+import '../ChatRoom/ChatRoom.css'
+export default function ChatWindow({ groupData }) {
+  const { selectedGroup } = useContext(SelectedGroupContext);
+  const [isGroupInfoVisible, setIsGroupInfoVisible] = useState(false);
+  const [messages, setMessages] = useState([]);
+
+ 
+
+  const handleGroupInfoToggle = () => {
+    setIsGroupInfoVisible(!isGroupInfoVisible);
+  };
 
   return (
-    <div style={{ textAlign: 'right' }}>
-       {/* Add the Link component with the destination path */}
-        <Button
-         type="primary"
-         onClick={handleGroupInfoToggle}
-        >
-          <HomeOutlined /> Home
-        </Button>
-        {isGroupInfoVisible && <GroupInfo onClose={handleGroupInfoToggle}/>}
+    <div className='chat-room' style={{ textAlign: 'right' }}>
+      {/* {messages.map((message, index) => (
+        <div key={index} className="chat-message">
+          <div className="message-header">
+            <span className="sender">{message.sender}</span>
+            <span className="timestamp">
+              {new Date(message.timestamp).toLocaleString()}
+            </span>
+          </div>
+          <div className="message-content">{message.content}</div>
+        </div>
+      ))} */}
+      {/* <h1>{selectedGroup.GroupId}</h1> */}
+      {selectedGroup && <ChatRoom groupId={selectedGroup.GroupId} />}
     </div>
   );
 }
